3528ede4b5a1d1daf17afa4972c54a6dc7a784c7
1BlueSparkle1 <kukuruzum@mail.ru>
2023-10-11 15:09:43 +0300

    Create Frame, add BD, begun layout

--- StoreForTechnology/MainWindow.xaml.cs
+++ StoreForTechnology/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using StoreForTechnology.MyPages;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using StoreForTechnology.Components;
+
 
 namespace StoreForTechnology
 {
@@ -23,6 +27,17 @@ namespace StoreForTechnology
         public MainWindow()
         {
             InitializeComponent();
+
+            var path = @"C:\Users\Acer\Desktop\РПМ\Задание магазин техники\";
+            foreach (var item in App.db.Product.ToArray())
+            {
+                var fullPath = path + item.MainImagePath.Trim();
+                var imageByte = File.ReadAllBytes(fullPath);
+                item.MainImage = imageByte;
+            }
+            App.db.SaveChanges();
+
+            MainFrame.Navigate(new ProductListPage());
         }
     }
 }
---
+++ StoreForTechnology/MyPages/ProductListPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using StoreForTechnology.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StoreForTechnology.MyPages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListPage.xaml
+    /// </summary>
+    public partial class ProductListPage : Page
+    {
+        public ProductListPage()
+        {
+            InitializeComponent();
+
+        //    var products = App.db.Product.ToList();
+        //    foreach (var product in products)
+        //    {
+        //        ProductWp.Children.Add(new ProductUserControl(new Image(), product.Title, product.Cost, product.CostTime.ToString(), product.CostVisibility));
+        //    }
+        }
+    }
+}
